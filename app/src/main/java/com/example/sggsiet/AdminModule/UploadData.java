d500465a1f9f686ece7281276bc7aa527abbe681
KaranBankar <karanbankar54@gmail.com>
2025-02-20 14:38:15 +0530

    Bug Solved

--- app/src/main/java/com/example/sggsiet/AdminModule/UploadData.java
+++ app/src/main/java/com/example/sggsiet/AdminModule/UploadData.java
@@ -1,5 +1,7 @@
 package com.example.sggsiet.AdminModule;
 
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
