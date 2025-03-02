7b7fb6bb9b45097e38ba851c8c1d77e1c71a5ed4
KaranBankar <karanbankar54@gmail.com>
2025-03-02 19:40:24 +0530

    Toast Added

--- app/src/main/java/com/example/sggsiet/FacultyModule/FacultyCheatRecordsActivity.kt
+++ app/src/main/java/com/example/sggsiet/FacultyModule/FacultyCheatRecordsActivity.kt
@@ -74,6 +74,7 @@ class FacultyCheatRecordsActivity : AppCompatActivity() {
                 dialog.dismiss()
             } else {
                 Toast.makeText(this, "Please fill all fields and upload an image", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show();
             }
         }
 
